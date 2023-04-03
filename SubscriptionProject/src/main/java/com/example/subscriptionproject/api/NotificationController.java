a6c2567468a380d5d831d7c05244a329ad424428
kshissh <egorushkina.katya@ya.ru>
2023-04-03 14:06:20 +0200

    Updating

--- SubscriptionProject/src/main/java/com/example/subscriptionproject/api/NotificationController.java
+++ SubscriptionProject/src/main/java/com/example/subscriptionproject/api/NotificationController.java
@@ -1,13 +1,10 @@
 package com.example.subscriptionproject.api;
 
-import com.example.subscriptionproject.DTOs.SubCreationDTO;
-import com.example.subscriptionproject.model.Transport;
 import com.example.subscriptionproject.notification.Notification;
 import com.example.subscriptionproject.notification.NotificationService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -23,16 +20,17 @@ public class NotificationController {
     @PostMapping("/sendnotification")
     public ResponseEntity<Notification> sendNotification() {
         Notification notification = new Notification();
-        notification = notificationService.sendNotificationToClient(notification);
         RestTemplate restTemplate = new RestTemplate();
         List<String> endpoints = Arrays.asList(
                 "https://webhook.site/3337c25a-7f4a-4b5a-8f14-fec98c7f5f41",
                 "https://webhook.site/014484e6-a5a5-4eb9-9761-acb916450fcf"
         );
         for (String endpoint : endpoints) {
+            notification = notificationService.sendNotificationToClient(notification);
             restTemplate.postForLocation(endpoint, notification);
         }
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
 
 }
