74d22e6d6313bbbe8306926997f65d8ea6b3c387
Ekaterina Egorushkina <ekaterina.egorushkina@mera.com>
2023-05-03 12:35:15 +0200

    Updatin

--- SpringBoot_localhost/NotificationDispatcher/src/main/java/com/example/notificationdispatcher/NotificationService.java
+++
@@ -1,9 +0,0 @@
-package com.example.notificationdispatcher;
-
-import com.example.notificationservice.model.Subscription;
-
-import java.util.List;
-
-public interface NotificationService {
-    List<Subscription> getAll();
-}
