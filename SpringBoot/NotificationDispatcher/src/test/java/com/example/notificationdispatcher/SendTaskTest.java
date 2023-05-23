51d84e7987f40664e8cb5b7547ef724b295cd4e0
Ekaterina Egorushkina <ekaterina.egorushkina@mera.com>
2023-05-23 11:08:10 +0200

    Updating tests

--- SpringBoot/NotificationDispatcher/src/test/java/com/example/notificationdispatcher/SendTaskTest.java
+++ SpringBoot/NotificationDispatcher/src/test/java/com/example/notificationdispatcher/SendTaskTest.java
@@ -17,50 +17,44 @@ import static org.mockito.Mockito.when;
 
 public class SendTaskTest extends TestCase {
     private RestTemplate restTemplate;
-    private String endpoint;
-    private String notification;
+    private final String ENDPOINT = "https://webhook.site/cfb71d4c-1ee4-40eb-9ae5-895309e5d636";
+    private final String NOTIFICATION = "{\n" +
+            "  \"event\": \"Notification\",\n" +
+            "  \"id\": \"db225502-2c15-45d6-a3c5-1f50ec787800\"\n" +
+            "}";
     private int attempt;
     private ScheduledExecutorConfig scheduledExecutor;
     private SendTask sendTask;
 
+
     @BeforeEach
     public void setup() {
-        sendTask = new SendTask(restTemplate, endpoint, notification, attempt, scheduledExecutor);
+        sendTask = new SendTask(restTemplate, ENDPOINT, NOTIFICATION, attempt, scheduledExecutor);
     }
 
     @Test
     public void testRunSuccess() {
-        String endpoint = "https://webhook.site/83d1ebc9-0f27-43a4-93c2-6c3a41b333b6";
-        String notification = "{\n" +
-                "  \"event\": \"Notification\",\n" +
-                "  \"id\": \"db225502-2c15-45d6-a3c5-1f50ec787800\"\n" +
-                "}";
         restTemplate = mock(RestTemplate.class);
-        sendTask = new SendTask(restTemplate, endpoint, notification, 0, scheduledExecutor);
+        sendTask = new SendTask(restTemplate, ENDPOINT, NOTIFICATION, 0, scheduledExecutor);
 
-        when(restTemplate.postForLocation(endpoint, notification)).thenReturn(null);
+        when(restTemplate.postForLocation(ENDPOINT, NOTIFICATION)).thenReturn(null);
 
         sendTask.run();
 
-        verify(restTemplate).postForLocation(endpoint, notification);
+        verify(restTemplate).postForLocation(ENDPOINT, NOTIFICATION);
     }
 
     @Test
     public void testRunFail() {
-        String endpoint = "https://webhook.site/cfb71d4c-1ee4-40eb-9ae5-895309e5d636";
-        String notification = "{\n" +
-                "  \"event\": \"Notification\",\n" +
-                "  \"id\": \"db225502-2c15-45d6-a3c5-1f50ec787800\"\n" +
-                "}";
         restTemplate = mock(RestTemplate.class);
         scheduledExecutor = mock(ScheduledExecutorConfig.class);
-        sendTask = new SendTask(restTemplate, endpoint, notification, 0, scheduledExecutor);
+        sendTask = new SendTask(restTemplate, ENDPOINT, NOTIFICATION, 0, scheduledExecutor);
 
-        when(restTemplate.postForLocation(endpoint, notification)).thenThrow(new RuntimeException("Error sending notification"));
+        when(restTemplate.postForLocation(ENDPOINT, NOTIFICATION)).thenThrow(new RuntimeException("Error sending notification"));
 
         ScheduledExecutorService executorService = mock(ScheduledExecutorService.class);
         when(scheduledExecutor.scheduledExecutorService()).thenReturn(executorService);
-        
+
         sendTask.run();
 
         verify(scheduledExecutor.scheduledExecutorService()).schedule(
@@ -68,14 +62,9 @@ public class SendTaskTest extends TestCase {
     }
     @Test
     public void testRunMaxRetryAttemptsReached() {
-        String endpoint = "https://webhook.site/cfb71d4c-1ee4-40eb-9ae5-895309e5d636";
-        String notification = "{\n" +
-                "  \"event\": \"Notification\",\n" +
-                "  \"id\": \"db225502-2c15-45d6-a3c5-1f50ec787800\"\n" +
-                "}";
         restTemplate = mock(RestTemplate.class);
         scheduledExecutor = mock(ScheduledExecutorConfig.class);
-        sendTask = new SendTask(restTemplate, endpoint, notification, 4, scheduledExecutor);
+        sendTask = new SendTask(restTemplate, ENDPOINT, NOTIFICATION, 4, scheduledExecutor);
 
         ScheduledExecutorService executorService = mock(ScheduledExecutorService.class);
         when(scheduledExecutor.scheduledExecutorService()).thenReturn(executorService);
