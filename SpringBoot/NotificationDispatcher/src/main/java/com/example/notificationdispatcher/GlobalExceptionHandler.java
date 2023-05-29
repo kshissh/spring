15d1b6f9d2e1f891e277a25ccc6ffacf197883e9
Ekaterina Egorushkina <ekaterina.egorushkina@mera.com>
2023-05-29 18:38:37 +0200

    Updating

--- SpringBoot/NotificationDispatcher/src/main/java/com/example/notificationdispatcher/GlobalExceptionHandler.java
+++ SpringBoot/NotificationDispatcher/src/main/java/com/example/notificationdispatcher/GlobalExceptionHandler.java
@@ -7,21 +7,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.io.IOException;
-/*
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
     private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
-
-    @ExceptionHandler(IOException.class)
-    public ResponseEntity<String> handleIOException(IOException ex) {
-        LOGGER.error("IOException occurred: {}", ex.getMessage());
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred");
-    }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception ex) {
         LOGGER.error("An unexpected error occurred: {}", ex.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred");
     }
-} */
+}
